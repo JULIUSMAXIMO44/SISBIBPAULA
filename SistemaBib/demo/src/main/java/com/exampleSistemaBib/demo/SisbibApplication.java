af9d9f926fa009127b3ddda6869ea205dd9a1485
JULIUSMAXIMO44 <jrudass@unicartagena.edu.co>
2024-11-07 12:51:13 -0500

    Add files via upload
    Componentes basicos del Proyecto  en Springboot de Gestion para un Sistema de biblioteca

---
+++ SistemaBib/demo/src/main/java/com/exampleSistemaBib/demo/SisbibApplication.java
@@ -0,0 +1,13 @@
+package com.exampleSistemaBib.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SisbibApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SisbibApplication.class, args);
+	}
+
+}
